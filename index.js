36df8e10c23fc1ab700e227d70ac3f14960096a6
Dhananjay-FullStack <sonidhanesh007@gmail.com>
2020-08-13 14:07:09 +0530

    minor changes

--- index.js
+++ index.js
@@ -28,12 +28,13 @@ $(document).ready(function () {
     $(this).parent('li').addClass('active');
   });
 
-
-
+  setTimeout(function () {
+    $('.loader-wrapper').fadeOut();
+}, 1000);
   // Loader code below
-  $(window).on("load", function () {
-    $(".loader-wrapper").fadeOut("slow");
-  });
+  // $(window).on("load", function () {
+  //   $(".loader-wrapper").fadeOut("slow");
+  // });
 
 
 
@@ -45,9 +46,12 @@ $(document).ready(function () {
   });
 
   //Adding and removing active class on nav-items
-  $(document).on('click', 'ul li', function () {
-    $(this).addClass('active').siblings().removeClass('active');
-  });
+  // $(document).on('click', 'ul li', function () {
+  //   $(this).addClass('active').siblings().removeClass('active');
+  //   $(window).scroll(function(event){
+  //     $(this).removeClass('active');
+  //   });
+  // });
 
 
   // Scroll to about section on click on span
@@ -77,10 +81,49 @@ btn.on('click', function (e) {
 });
 
 
-//add black background on navbar on scroll down
-$(window).scroll(function () {
-  $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+
+
+
+
+
+
+
+
+var sections = $('section')
+  , nav = $('nav')
+  , nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+  
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+    
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+      
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
 });
+
+nav.find('a').on('click', function () {
+  var $el = $(this)
+    , id = $el.attr('href');
+  
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 100);
+  
+  return false;
+});
+
+
+
+
 
 
 });
